158fb3b84f9d6ac44c955ddd77235fcf3821e73a
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2023-12-23 14:38:29 -0700

    fix stronghold distance inconsistency

--- main.py
+++ main.py
@@ -184,6 +184,8 @@ def get_first_three_starts_no_biomes(seed):
     seed = init(seed)
     seed, rand_0 = next_double(seed)
     seed, rand_1 = next_double(seed)
+    seed, rand_2 = next_double(seed)
+    seed, rand_3 = next_double(seed)
     angle = rand_0 * np.pi * np.float64(2)
     distance_ring = np.float64(4) * np.float64(32) + (
         rand_1 - np.float64(0.5)
@@ -192,13 +194,13 @@ def get_first_three_starts_no_biomes(seed):
     chunk_z_0 = np.int64(np.round(np.sin(angle) * distance_ring))
     angle += np.pi * 2.0 / 3.0
     distance_ring = np.float64(4) * np.float64(32) + (
-        rand_1 - np.float64(0.5)
+        rand_2 - np.float64(0.5)
     ) * np.float64(32) * np.float64(2.5)
     chunk_x_1 = np.int64(np.round(np.cos(angle) * distance_ring))
     chunk_z_1 = np.int64(np.round(np.sin(angle) * distance_ring))
     angle += np.pi * 2.0 / 3.0
     distance_ring = np.float64(4) * np.float64(32) + (
-        rand_1 - np.float64(0.5)
+        rand_3 - np.float64(0.5)
     ) * np.float64(32) * np.float64(2.5)
     chunk_x_2 = np.int64(np.round(np.cos(angle) * distance_ring))
     chunk_z_2 = np.int64(np.round(np.sin(angle) * distance_ring))
