d9867daf1d9e738b2ec52f406da44c2b06449821
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2023-12-24 08:13:14 -0700

    abandon all hope; hardcode includes

--- setup.py
+++ setup.py
@@ -1,22 +1,43 @@
-import pkgutil
 import sys
 
-import pynput.keyboard
-import pynput.mouse
 from cx_Freeze import Executable, setup
 
 includes = [
-    "pynput.keyboard." + sub.name
-    for sub in pkgutil.iter_modules(pynput.keyboard.__path__)
-] + ["pynput.mouse." + sub.name for sub in pkgutil.iter_modules(pynput.mouse.__path__)]
+    "pynput.keyboard._base",
+    "pynput.keyboard._darwin",
+    "pynput.keyboard._dummy",
+    "pynput.keyboard._uinput",
+    "pynput.keyboard._win32",
+    "pynput.keyboard._xorg",
+    "pynput.mouse._base",
+    "pynput.mouse._darwin",
+    "pynput.mouse._dummy",
+    "pynput.mouse._win32",
+    "pynput.mouse._xorg",
+]
 
 if sys.platform == "linux":
-    import Xlib.keysymdef
-
-    includes += [
-        "Xlib.keysymdef." + sub.name
-        for sub in pkgutil.iter_modules(Xlib.keysymdef.__path__)
-    ]
+    includes += (
+        "Xlib.keysymdef.apl",
+        "Xlib.keysymdef.arabic",
+        "Xlib.keysymdef.cyrillic",
+        "Xlib.keysymdef.greek",
+        "Xlib.keysymdef.hebrew",
+        "Xlib.keysymdef.katakana",
+        "Xlib.keysymdef.korean",
+        "Xlib.keysymdef.latin1",
+        "Xlib.keysymdef.latin2",
+        "Xlib.keysymdef.latin3",
+        "Xlib.keysymdef.latin4",
+        "Xlib.keysymdef.miscellany",
+        "Xlib.keysymdef.publishing",
+        "Xlib.keysymdef.special",
+        "Xlib.keysymdef.technical",
+        "Xlib.keysymdef.thai",
+        "Xlib.keysymdef.xf86",
+        "Xlib.keysymdef.xk3270",
+        "Xlib.keysymdef.xkb",
+    )
 
 
 build_exe_options = {
